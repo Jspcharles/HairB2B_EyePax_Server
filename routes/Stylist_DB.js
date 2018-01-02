5aecb90bf15e0a543781c73053393a7756e8bfd4
Jspcharles <jspcharles@gmail.com>
2018-01-02 18:38:02 +0530

    search field functions and quries are developed

--- routes/Stylist_DB.js
+++ routes/Stylist_DB.js
@@ -214,6 +214,16 @@ router.get('/stylist_details/:id', function(req, res) {
         stylistLists.push(stylist);
     }
 
+    stylistLists.sort(function (a, b) {
+        if (a.rating > b.rating){
+            return -1;
+        }
+        else if (a.rating === b.rating){
+            return 0;
+        }
+        return 1;
+    });
+
     console.log(stylistLists);
     res.setHeader('Content-Type', 'application/json');
     res.send(stylistLists);
@@ -274,6 +284,16 @@ router.get('/stylist_details/bylocation/:location', function(req, res) {
 
         stylistLists.push(stylist);
     }
+
+    stylistLists.sort(function (a, b) {
+        if (a.rating > b.rating){
+            return -1;
+        }
+        else if (a.rating === b.rating){
+            return 0;
+        }
+        return 1;
+    });
 
     console.log(stylistLists);
     res.setHeader('Content-Type', 'application/json');
@@ -340,6 +360,95 @@ router.get('/stylist_details/bySkill/:skill', function(req, res) {
         stylistLists.push(stylist);
     }
 
+    stylistLists.sort(function (a, b) {
+        if (a.rating > b.rating){
+            return -1;
+        }
+        else if (a.rating === b.rating){
+            return 0;
+        }
+        return 1;
+    });
+
+    // console.log(stylistLists);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(stylistLists);
+});
+
+router.get('/stylist_details/byName/:name', function(req, res) {
+    var name = req.params.name;
+    var first_name = name.split(' ')[0];
+    var last_name = name.split(' ')[1];
+
+    var output = syncSql.mysql(
+        {
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'hairb2b_db',
+            port: '3306'
+        },
+        // "select * from trn_stylist where address_line_1 = '" +req.params.skill+"'"
+
+        "select * from trn_stylist where first_name like '%"+first_name+"%' or last_name like '%"+last_name+"%'"
+    );
+    console.log(req.params.skill);
+
+    // console.log(output.data.rows);s
+    // res.send(output.data.rows);
+
+    var stylistLists = [];
+
+    for (var i = 0; i < output.data.rows.length; i++){
+        var stylist = {};
+        stylist.id = output.data.rows[i].id;
+        stylist.first_name = output.data.rows[i].first_name;
+        stylist.last_name = output.data.rows[i].last_name;
+        stylist.profile_pic = base64_encode(path.resolve(output.data.rows[i].profile_pic));
+        stylist.address_line_1 = output.data.rows[i].address_line_1;
+        stylist.address_line_2 = output.data.rows[i].address_line_2;
+        stylist.city = output.data.rows[i].city;
+        stylist.state = output.data.rows[i].state;
+        stylist.mrng_cost = output.data.rows[i].mrng_cost;
+        stylist.evng_cost = output.data.rows[i].evng_cost;
+        stylist.telephone = output.data.rows[i].telephone;
+        stylist.description = output.data.rows[i].description;
+        stylist.rating = output.data.rows[i].rating;
+        stylist.terms_and_condotions = output.data.rows[i].terms_and_condotions;
+        stylist.skill = [];
+
+        var r = syncSql.mysql(
+            {
+                host: 'localhost',
+                user: 'root',
+                password: '',
+                database: 'hairb2b_db',
+                port: '3306'
+            },
+            "select * from trn_skill where stylist_id = "+stylist.id
+        );
+
+        for (var j =0 ; j<r.data.rows.length;j++){
+
+            stylist.skill.push(r.data.rows[j].skill);
+        }
+        console.log('-----------');
+        console.log(stylist.id);
+        console.log(stylist.skill);
+        console.log('-----------');
+        stylistLists.push(stylist);
+    }
+
+    stylistLists.sort(function (a, b) {
+        if (a.rating > b.rating){
+            return -1;
+        }
+        else if (a.rating === b.rating){
+            return 0;
+        }
+        return 1;
+    });
+
     // console.log(stylistLists);
     res.setHeader('Content-Type', 'application/json');
     res.send(stylistLists);
@@ -373,74 +482,6 @@ router.get('/stylist_details/bySkill/:skill', function(req, res) {
     //
     // });
 
-router.get('/stylist_details/namefiltered', function(req, res) {
-    var output = syncSql.mysql(
-        {
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'hairb2b_db',
-            port: '3306'
-        },
-        "select * from trn_stylist"
-    );
-
-    // console.log(output.data.rows);
-    // res.send(output.data.rows);
-
-    var stylistLists = [];
-
-    for (var i = 0; i < output.data.rows.length; i++){
-        var stylist = {};
-        stylist.id = output.data.rows[i].id;
-        stylist.first_name = output.data.rows[i].first_name;
-        stylist.last_name = output.data.rows[i].last_name;
-        stylist.profile_pic = base64_encode(path.resolve(output.data.rows[i].profile_pic));
-        stylist.address_line_1 = output.data.rows[i].address_line_1;
-        stylist.address_line_2 = output.data.rows[i].address_line_2;
-        stylist.city = output.data.rows[i].city;
-        stylist.state = output.data.rows[i].state;
-        stylist.mrng_cost = output.data.rows[i].mrng_cost;
-        stylist.evng_cost = output.data.rows[i].evng_cost;
-        stylist.telephone = output.data.rows[i].telephone;
-        stylist.description = output.data.rows[i].description;
-        stylist.rating = output.data.rows[i].rating;
-        stylist.terms_and_condotions = output.data.rows[i].terms_and_condotions;
-        stylist.skill = [];
-
-        var r = syncSql.mysql(
-            {
-                host: 'localhost',
-                user: 'root',
-                password: '',
-                database: 'hairb2b_db',
-                port: '3306'
-            },
-            "select * from trn_skill where  stylist_id = "+stylist.id
-        );
-        for (var j =0 ; j<r.data.rows.length;j++){
-
-            stylist.skill.push(r.data.rows[j].skill);
-        }
-        console.log(r);
-
-        stylistLists.push(stylist);
-    }
-
-    stylistLists.sort(function (a, b) {
-        if (a.rating > b.rating){
-            return -1;
-        }
-        else if (a.rating === b.rating){
-            return 0;
-        }
-        return 1;
-    });
-
-    console.log(stylistLists);
-    res.setHeader('Content-Type', 'application/json');
-    res.send(stylistLists);
-});
 
 
 
